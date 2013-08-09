88123cb7ee0ce48058e84f6cedd85046da736be5
Simon Busch <morphis@gravedo.de>
2013-08-09 16:16:16 +0000

    Implement a domain chooser box and rebuild application list dynamically
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/com/android/securecontainer/SecureContainerMainActivity.java
+++ src/com/android/securecontainer/SecureContainerMainActivity.java
@@ -8,56 +8,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.BaseAdapter;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ListAdapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.Spinner;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
-public class SecureContainerMainActivity extends Activity {
+public class SecureContainerMainActivity extends Activity
+										 implements OnItemSelectedListener {
 	private static final String TAG = "SecureContainer";
-	private static final String SEINFO_PLATFORM = "platform";
-	private static final String SEINFO_SYSTEM = "system";
-	private static final String SEINFO_CONTAINER = "container";
-	private static final String SEINFO_UNTRUSTED = "untrusted";
+
+	private ArrayList<String> mApplicationList;
+	private ArrayAdapter mApplicationListAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
 
-		final PackageManager pm = getPackageManager();
-
-		final ArrayList<String> containerApplications = new ArrayList<String>();
-
-		List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-		for (ApplicationInfo packageInfo : packages) {
-			Log.d(TAG, "Installed package :" + packageInfo.packageName);
-			Log.d(TAG,
-					"Launch Activity :"
-							+ pm.getLaunchIntentForPackage(packageInfo.packageName));
-			Log.d(TAG, "seinfo :" + packageInfo.seinfo);
-
-			if (packageInfo.seinfo.equals(SEINFO_CONTAINER) &&
-				// NOTE using this for determination wether the package contains
-				// a launchable component is rather ugly and slow but works for our
-				// purpose
-				pm.getLaunchIntentForPackage(packageInfo.packageName) != null) {
-
-				Log.d(TAG, "Found platform application");
-				containerApplications.add(packageInfo.packageName);
-			}
-		}
+		mApplicationList = new ArrayList<String>();
 
 		final ListView applicationListView = (ListView) findViewById(R.id.application_list);
-		final StableArrayAdapter adapter = new StableArrayAdapter(this,
-				android.R.layout.simple_list_item_1, containerApplications);
+		mApplicationListAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, mApplicationList);
 
-		applicationListView.setAdapter(adapter);
+		applicationListView.setAdapter(mApplicationListAdapter);
 		applicationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, final View view,
@@ -68,33 +50,54 @@ public class SecureContainerMainActivity extends Activity {
 					Toast.makeText(getBaseContext(), R.string.app_launch_failed, Toast.LENGTH_SHORT).show();
 					return;
 				}
-				
+
 				startActivity(appLaunchIntent);
 			}
 
 		});
+
+		Spinner domainListSpinner = (Spinner) findViewById(R.id.domain_filter);
+		ArrayAdapter<CharSequence> domainListAdapter = ArrayAdapter.createFromResource(this, R.array.domain_types,
+										android.R.layout.simple_spinner_item);
+		domainListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		domainListSpinner.setAdapter(domainListAdapter);
+		domainListSpinner.setOnItemSelectedListener(this);
 	}
 
-	private class StableArrayAdapter extends ArrayAdapter<String> {
-		HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+	private void fillApplicationListForCategory(String category) {
+		final PackageManager pm = getPackageManager();
 
-		public StableArrayAdapter(Context context, int textViewResourceId,
-				List<String> objects) {
-			super(context, textViewResourceId, objects);
-			for (int i = 0; i < objects.size(); ++i) {
-				mIdMap.put(objects.get(i), i);
+		mApplicationList.clear();
+
+		List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+		for (ApplicationInfo packageInfo : packages) {
+			Log.d(TAG, "Installed package :" + packageInfo.packageName);
+			Log.d(TAG, "Launch Activity :"
+				+ pm.getLaunchIntentForPackage(packageInfo.packageName));
+			Log.d(TAG, "seinfo :" + packageInfo.seinfo);
+
+			for (String mmacType : packageInfo.mmacTypes) {
+				Log.d(TAG, "mmacType :" + mmacType);
+			}
+
+			if (packageInfo.seinfo.equals(category) &&
+				// NOTE using this for determination wether the package contains
+				// a launchable component is rather ugly and slow but works for our
+				// purpose
+				pm.getLaunchIntentForPackage(packageInfo.packageName) != null) {
+
+				Log.d(TAG, "Found platform application");
+				mApplicationList.add(packageInfo.packageName);
 			}
 		}
+	}
 
-		@Override
-		public long getItemId(int position) {
-			String item = getItem(position);
-			return mIdMap.get(item);
-		}
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+		String category = (String) parent.getItemAtPosition(pos);
+		fillApplicationListForCategory(category);
+		mApplicationListAdapter.notifyDataSetChanged();
+	}
 
-		@Override
-		public boolean hasStableIds() {
-			return true;
-		}
+	public void onNothingSelected(AdapterView<?> parent) {
 	}
 }
